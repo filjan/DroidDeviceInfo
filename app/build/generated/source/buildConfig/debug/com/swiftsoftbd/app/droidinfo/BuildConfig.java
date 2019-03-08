63a2ab1c942a41821dfa5f2ec8dae8f641b8b8e9
Tod <tfiler@filjan.com>
2019-03-07 18:18:24 -0700

    Update code

---
+++ app/build/generated/source/buildConfig/debug/com/swiftsoftbd/app/droidinfo/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.swiftsoftbd.app.droidinfo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.swiftsoftbd.app.droidinfo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "2.1.42.1.4";
+}
