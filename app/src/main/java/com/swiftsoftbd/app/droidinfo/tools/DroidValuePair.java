feace9f9b6bb3007531d1010e3385da3a1f6a551
Tod Filer <tod.filer@beaumont.org>
2023-06-27 13:52:54 -0700

    add jetifier

---
+++ app/src/main/java/com/swiftsoftbd/app/droidinfo/tools/DroidValuePair.java
@@ -0,0 +1,11 @@
+package com.swiftsoftbd.app.droidinfo.tools;
+
+public class DroidValuePair {
+    private String firstValue;
+    private String secondValue;
+
+    public String getFirstValue() {return this.firstValue; }
+    public void setFirstValue(String value) {this.firstValue = value; }
+    public String getSecondValue() {return this.secondValue; }
+    public void setSecondValue(String value) {this.secondValue = value; }
+}
